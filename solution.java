3ebd87b29148720c097afe96c9b3e9a8bb9c45f6
Kooreis <kooreis.official@gmail.com>
2024-02-14 04:57:56 -0600

    In this first part of the script, the necessary libraries are imported and the main class is defined. A scanner object is created to read the user's input from the console. The user is prompted to enter a sentence, which is then stored in a string variable.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a simple Java console application that implements a word frequency counter using a stream of words:
-
-```java
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,22 +8,5 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a sentence:");
         String sentence = scanner.nextLine();
-        String[] words = sentence.split("\\s+");
-        Map<String, Long> wordCount = new HashMap<>();
-
-        Arrays.stream(words).forEach(word -> {
-            word = word.toLowerCase();
-            if (wordCount.containsKey(word)) {
-                wordCount.put(word, wordCount.get(word) + 1);
-            } else {
-                wordCount.put(word, 1L);
-            }
-        });
-
-        System.out.println("Word Frequencies:");
-        wordCount.forEach((word, count) -> System.out.println(word + ": " + count));
     }
 }
-```
-
-This program reads a sentence from the console, splits it into words, and then uses a stream to process each word. For each word, it checks if the word is already in the map. If it is, it increments the count. If it's not, it adds the word to the map with a count of 1. Finally, it prints out the frequencies of each word.
