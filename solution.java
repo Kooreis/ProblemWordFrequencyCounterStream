b412e8509c1bebbd49911b38305754e9fb6176f9
Kooreis <kooreis.official@gmail.com>
2024-02-15 04:57:56 -0600

    In this update, the sentence entered by the user is split into individual words using the split method. A new HashMap is created to store the words and their frequencies.

--- solution.java
+++ solution.java
@@ -1,12 +1,2 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a sentence:");
-        String sentence = scanner.nextLine();
-    }
-}
+String[] words = sentence.split("\s+");
+Map<String, Long> wordCount = new HashMap<>();
